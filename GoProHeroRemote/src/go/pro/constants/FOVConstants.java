752779f21336c05b799ce4de4b35ff6881a00950
shymi <sshymi@yahoo.com>
2014-08-01 11:40:35 +0300

    Constants for video and FOV added

---
+++ GoProHeroRemote/src/go/pro/constants/FOVConstants.java
@@ -0,0 +1,11 @@
+package go.pro.constants;
+
+public class FOVConstants {
+	//command
+	private static String FOV_COMMAND = "FV";
+	
+	//values
+	private static String FOV_WIDE = "00";
+	private static String FOV_MEDIUM = "01";
+	private static String FOV_NARROW = "02";
+}
