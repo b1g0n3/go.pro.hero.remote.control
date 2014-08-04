b8b28155cef5ee4ce16c6b17694c3bf00cd07cca
shymi <sshymi@yahoo.com>
2014-08-04 15:30:37 +0300

    Fixed commands

--- GoProHeroRemote/src/go/pro/constants/CommandConstants.java
+++ GoProHeroRemote/src/go/pro/constants/CommandConstants.java
@@ -3,15 +3,15 @@ package go.pro.constants;
 public class CommandConstants {
 	public static String POWER_COMMAND_BACPAC = "PW";
 	
-	public static String POWER_ON = "00";
-	public static String POWER_OFF = "01";
+	public static String POWER_ON = "01";
+	public static String POWER_OFF = "00";
 	
-	public static String SHUTTER_COMMAND_BACPAC = "PW";
+	public static String SHUTTER_COMMAND_BACPAC = "SH";
 	
 	public static String SHUTTER_SHUTTER = "01";
 	public static String SHUTTER_STOP = "00";
 	
-	public static String PREVIEW_COMMAND_CAMERA = "PW";
+	public static String PREVIEW_COMMAND_CAMERA = "PV";
 	
 	public static String PREVIEW_ON = "02";
 	public static String PREVIEW_OFF = "00";
