d0c0c5b641c8b766ca2a44a95f4ef6ae1d3a2fff
shymi <sshymi@yahoo.com>
2014-08-26 12:00:01 +0300

    Changed to proper methods

--- GoProHeroRemote/src/go/pro/actions/RecordVideo.java
+++ GoProHeroRemote/src/go/pro/actions/RecordVideo.java
@@ -1,72 +1,40 @@
 package go.pro.actions;
 
 import go.pro.constants.CommandConstants;
+import go.pro.constants.CommonConstants;
 import go.pro.constants.ModeConstants;
 import go.pro.constants.VideoResolutionConstants;
 import go.pro.utils.CommandBuildingUtils;
 
-import java.io.FileWriter;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Date;
 
 public class RecordVideo {
-
-	public static void RecordVideoStream(String password, int quality, long maxDurationInMillis) {
+	
+	public static boolean changeToVideoMode(String password) {
 		String status = RetrieveCameraStatus.RetrieveStatus(password);
 		if(status == null) {
-			return;
+			//Camera is turned off/phone not connected to camer via Wi-Fi
+			return false;
 		}
+		
 		HttpURLConnection conn = null;
-		FileWriter fr = null;
 		try {
+			//Check if camera is in video mode and send command to change to video mode if it is not
 			if(!status.substring(2, 4).equals("00")) {
 				conn = (HttpURLConnection) (new URL(CommandBuildingUtils.generateCommandURL(ModeConstants.MODE_COMMAND_CAMERA, ModeConstants.MODE_VIDEO, password, "camera"))).openConnection();
-				conn.getResponseCode();
-				conn.disconnect();
-			}
-			fr = new FileWriter ("result.txt", true);
-			fr.write("-------------" + (new Date(System.currentTimeMillis())) + "\n");
-			
-			if(quality == VideoResolutionConstants.VIDEO_QUALITY_LOW) {
-				conn = (HttpURLConnection) (new URL(CommandBuildingUtils.generateCommandURL(VideoResolutionConstants.HERO3_BLACK_RESOLUTION_COMMAND_NEW_CAMERA, VideoResolutionConstants.HERO3_BLACK_RESOLUTION_WVGA_240_NEW, password, "camera"))).openConnection();
-			} else {
-				conn = (HttpURLConnection) (new URL(CommandBuildingUtils.generateCommandURL(VideoResolutionConstants.HERO3_BLACK_RESOLUTION_COMMAND_NEW_CAMERA, VideoResolutionConstants.HERO3_BLACK_RESOLUTION_720p_120_NEW, password, "camera"))).openConnection();
-			}
-			conn.getResponseCode();
-			conn.disconnect();			
-			Thread.sleep(1000);
-			
-			conn = (HttpURLConnection) (new URL(CommandBuildingUtils.generateCommandURL(CommandConstants.SHUTTER_COMMAND_BACPAC, CommandConstants.SHUTTER_SHUTTER, password, "bacpac"))).openConnection();
-			long start = 100000;
-			if(conn.getResponseCode() == 200) {
-				start = System.currentTimeMillis();
-				conn.disconnect();
-				fr.write(String.valueOf(start) + ": ");
-				fr.write(RetrieveCameraStatus.RetrieveStatus(password) + "\n");	
-				fr.flush();
-			}
-			Thread.sleep(5000);
-			while((System.currentTimeMillis() - start) < maxDurationInMillis){
-				if(RetrieveCameraStatus.RetrieveStatus(password).charAt(59) == '1'){
-					System.out.println((System.currentTimeMillis() - start)/1000);
-					fr.write(String.valueOf(System.currentTimeMillis()) + ": ");
-					fr.write(RetrieveCameraStatus.RetrieveStatus(password) + "\n");	
-					fr.flush();
-					Thread.sleep(2000);
+				if(conn.getResponseCode() == 200) {
+					conn.disconnect();
+					return true;
 				} else {
-					break;
+					conn.disconnect();
+					return false;
 				}
+			} else {
+				return true;
 			}
-			conn = (HttpURLConnection) (new URL(CommandBuildingUtils.generateCommandURL(CommandConstants.SHUTTER_COMMAND_BACPAC, CommandConstants.SHUTTER_STOP, password, "bacpac"))).openConnection();
-			conn.getResponseCode();
-			conn.disconnect();
-			Thread.sleep(1000);
-			fr.write("End ");
-			fr.write(RetrieveCameraStatus.RetrieveStatus(password) + "\n");
-			fr.flush();
 		} catch (MalformedURLException e) {
 			if(conn != null) {
 				conn.disconnect();
@@ -77,19 +45,131 @@ public class RecordVideo {
 				conn.disconnect();
 			}
 			e.printStackTrace();
-		} catch (InterruptedException e) {
+		}
+		return false;
+	}
+	
+	public static boolean changeVideoQuality(String password, int quality, int cameraModel) {
+		String status = RetrieveCameraStatus.RetrieveStatus(password);
+		if(status == null) {
+			//Camera is turned off/phone not connected to camer via Wi-Fi
+			return false;
+		}
+		
+		HttpURLConnection conn = null;
+		try {
+			//Check if camera is in video mode and send command to change to video mode if it is not
+			if(cameraModel == CommonConstants.CAMERA_HERO_2_3_SILVER) {
+				if(quality == VideoResolutionConstants.VIDEO_QUALITY_LOW) {
+					conn = (HttpURLConnection) (new URL(CommandBuildingUtils.generateCommandURL(VideoResolutionConstants.HERO23_RESOLUTION_COMMAND_CAMERA, VideoResolutionConstants.HERO23_RESOLUTION_WVGA_60, password, "camera"))).openConnection();
+				} else {
+					conn = (HttpURLConnection) (new URL(CommandBuildingUtils.generateCommandURL(VideoResolutionConstants.HERO23_RESOLUTION_COMMAND_CAMERA, VideoResolutionConstants.HERO23_RESOLUTION_720p_30, password, "camera"))).openConnection();
+				}
+			} else if(cameraModel == CommonConstants.CAMERA_HERO_3_BLACK) {
+				if(quality == VideoResolutionConstants.VIDEO_QUALITY_LOW) {
+					conn = (HttpURLConnection) (new URL(CommandBuildingUtils.generateCommandURL(VideoResolutionConstants.HERO3_BLACK_RESOLUTION_COMMAND_NEW_CAMERA, VideoResolutionConstants.HERO3_BLACK_RESOLUTION_WVGA_240_NEW, password, "camera"))).openConnection();
+				} else {
+					conn = (HttpURLConnection) (new URL(CommandBuildingUtils.generateCommandURL(VideoResolutionConstants.HERO3_BLACK_RESOLUTION_COMMAND_NEW_CAMERA, VideoResolutionConstants.HERO3_BLACK_RESOLUTION_720p_120_NEW, password, "camera"))).openConnection();
+				}
+			} else {
+				conn = (HttpURLConnection) (new URL(CommandBuildingUtils.generateCommandURL(VideoResolutionConstants.HERO3_PLUS_BLACK_RESOLUTION_COMMAND_WITHOUT_FPS_CAMERA, VideoResolutionConstants.HERO3_PLUS_BLACK_RESOLUTION_720p_WITHOUT_FPS, password, "camera"))).openConnection();
+			}
+			if(conn.getResponseCode() == 200) {
+				conn.disconnect();
+				return true;
+			} else {
+				conn.disconnect();
+				return false;
+			}
+		} catch (MalformedURLException e) {
+			if(conn != null) {
+				conn.disconnect();
+			}
+			e.printStackTrace();
+		} catch (IOException e) {
+			if(conn != null) {
+				conn.disconnect();
+			}
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public static boolean startRecordVideo(String password) {
+		String status = RetrieveCameraStatus.RetrieveStatus(password);
+		if(status == null) {
+			//Camera is turned off/phone not connected to camer via Wi-Fi
+			return false;
+		} else if (status.charAt(59) == '1' && status.substring(2, 4).equals("00")) {
+			//Camera is recording
+			return true;
+		}
+		
+		HttpURLConnection conn = null;
+		try {				
+			//Start recording
+			conn = (HttpURLConnection) (new URL(CommandBuildingUtils.generateCommandURL(CommandConstants.SHUTTER_COMMAND_BACPAC, CommandConstants.SHUTTER_SHUTTER, password, "bacpac"))).openConnection();
+			if(conn.getResponseCode() == 200) {
+				conn.disconnect();
+				return true;
+			} else {
+				conn.disconnect();
+				return false;
+			}
+		} catch (MalformedURLException e) {
+			if(conn != null) {
+				conn.disconnect();
+			}
+			e.printStackTrace();
+		} catch (IOException e) {
+			if(conn != null) {
+				conn.disconnect();
+			}
 			e.printStackTrace();
 		} finally {
-			try {
-				if(fr != null) {
-					fr.close();
-				}
-				if(conn != null) {
-					conn.disconnect();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
+			if(conn != null) {
+				conn.disconnect();
 			}
 		}
+		return false;
+	}
+	
+	public static boolean stopRecordVideo(String password) {
+		String status = RetrieveCameraStatus.RetrieveStatus(password);
+		if(status == null) {
+			//Camera is turned off/phone not connected to camer via Wi-Fi
+			return false;
+		} else if (status.charAt(59) == '0') {
+			//Camera is stopped
+			return true;
+		}
+		
+		HttpURLConnection conn = null;
+		try {		
+			//Start recording
+			conn = (HttpURLConnection) (new URL(CommandBuildingUtils.generateCommandURL(CommandConstants.SHUTTER_COMMAND_BACPAC, CommandConstants.SHUTTER_SHUTTER, password, "bacpac"))).openConnection();
+			if(conn.getResponseCode() == 200) {
+				conn.disconnect();
+				return true;
+			} else {
+				conn.disconnect();
+				return false;
+			}
+		} catch (MalformedURLException e) {
+			if(conn != null) {
+				conn.disconnect();
+			}
+			e.printStackTrace();
+		} catch (IOException e) {
+			if(conn != null) {
+				conn.disconnect();
+			}
+			e.printStackTrace();
+		} finally {
+			if(conn != null) {
+				conn.disconnect();
+			}
+		}
+		return false;
 	}
 }
