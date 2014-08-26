0e5dcd41732dee223684233f826b60db0dd1bdb8
shymi <sshymi@yahoo.com>
2014-08-26 12:01:54 +0300

    Proper methods are now used

--- GoProHeroRemote/src/go/pro/actions/RetrieveCameraStatus.java
+++ GoProHeroRemote/src/go/pro/actions/RetrieveCameraStatus.java
@@ -1,9 +1,7 @@
 package go.pro.actions;
 
-import go.pro.constants.CommandConstants;
 import go.pro.utils.CommandBuildingUtils;
 
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -17,65 +15,26 @@ public class RetrieveCameraStatus {
 		try {
 			URL url = new URL(CommandBuildingUtils.generateCommandURL("se", null, password, "camera"));
 			con =(HttpURLConnection) url.openConnection();
-	        con.setRequestMethod("GET");
 	        //check if camera is powered on and if it's not - send command to power on
 	        if(con.getResponseCode() != 200) {
 	        	 con.disconnect();
-	        	 int i = 0;
-	        	 while(i < 6) {
-	        		 con = (HttpURLConnection) (new URL(CommandBuildingUtils.generateCommandURL(CommandConstants.POWER_COMMAND_BACPAC, CommandConstants.POWER_ON, password, "bacpac"))).openConnection();
-	        		 con.setRequestMethod("GET");
-	        		 con.getResponseCode();
-	        		 con.disconnect();
-	        		 Thread.sleep(5000);
-	        		 con =(HttpURLConnection) url.openConnection();
-	        		 con.setRequestMethod("GET");
-	        		 if(con.getResponseCode() == 200) {
-	        			 break;
-	        		 }
-	        		 con.disconnect();
-	        		 i++;
-	        	 }
-	        	 if(i >= 6){
-	        		 System.out.println("Connection could not be established!");
-	            	 return null;
-	        	 }
-	         }
-	 		FileWriter fr = new FileWriter ("test.txt", true);
-	 		FileWriter frStr = new FileWriter ("teststr.txt", true);
-	        byte[] buff = new byte[31];
-	        InputStream is = con.getInputStream();
-	        while (is.read(buff) != -1) {
-	        	System.out.println("Retrieved status at" + System.currentTimeMillis());
-		    }
-	        con.disconnect();
-	        char[] hexChars = new char[buff.length * 2];
-	        for ( int j = 0; j < buff.length; j++ ) {
-	            int v = buff[j] & 0xFF;
-	            hexChars[j * 2] = hexArray[v >>> 4];
-	            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	        	 return null;
+	        } else {
+	        	byte[] buff = new byte[31];
+	        	InputStream is = con.getInputStream();
+		        while (is.read(buff) != -1) {
+		        	System.out.println("Retrieved status at" + System.currentTimeMillis());
+			    }
+		        con.disconnect();
+		        char[] hexChars = new char[buff.length * 2];
+		        for ( int j = 0; j < buff.length; j++ ) {
+		            int v = buff[j] & 0xFF;
+		            hexChars[j * 2] = hexArray[v >>> 4];
+		            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+		        }
+		        return String.valueOf(hexChars);
 	        }
-	        fr.write(hexChars);
-	        fr.write("\n");
-	        StringBuilder sb = new StringBuilder();
-	        for ( int j = 0; j < hexChars.length-1; j+=2 ) {
-	        	sb.append(hexChars[j]);
-	        	sb.append(hexChars[j+1]);
-	        	sb.append("|");
-	        }
-	        frStr.write(sb.toString());
-	        frStr.write("\n");
-	        fr.close();
-	        frStr.close();
-			return String.valueOf(hexChars);
-		} catch (InterruptedException e) {
-			if(con != null) {
-				con.disconnect();
-			}
-			e.printStackTrace();
-			return null;
 		} catch (IOException e) {
-			System.out.println("Connection could not be established!");
 			if(con != null) {
 				con.disconnect();
 			}
