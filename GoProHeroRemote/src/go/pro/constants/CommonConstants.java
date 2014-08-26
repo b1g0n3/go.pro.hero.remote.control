bf83a9b36d578e69b4b3005aedc7250aae44ac5e
shymi <sshymi@yahoo.com>
2014-08-26 12:02:47 +0300

    Common constants added

---
+++ GoProHeroRemote/src/go/pro/constants/CommonConstants.java
@@ -0,0 +1,9 @@
+package go.pro.constants;
+
+public class CommonConstants {
+	
+	public static int CAMERA_HERO_2_3_SILVER = 1;
+	public static int CAMERA_HERO_3_BLACK = 2;
+	public static int CAMERA_HERO_3_PLUS_BLACK = 3;
+	
+}
