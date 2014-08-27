c2a7d8ea2752e82a2880b336a363ec14d8733bcb
shymi <sshymi@yahoo.com>
2014-08-27 12:00:15 +0300

    Added file paths

--- GoProHeroRemote/src/go/pro/constants/CommonConstants.java
+++ GoProHeroRemote/src/go/pro/constants/CommonConstants.java
@@ -6,4 +6,7 @@ public class CommonConstants {
 	public static int CAMERA_HERO_3_BLACK = 2;
 	public static int CAMERA_HERO_3_PLUS_BLACK = 3;
 	
+	//Possible URLs for file directory
+	public static String fileDirURL1 = "http://10.5.5.9:8080/videos/DCIM/";
+	public static String fileDirURL2 = "http://10.5.5.9:8080/videos/DCIM/100GOPRO/";
 }
